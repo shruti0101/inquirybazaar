fa6b3193115453443bf52ceace0e492564bef40b
shruti0101 <shrutiguptacu@gmail.com>
2026-05-07 10:50:37 +0530

    chnges

--- components/Landingpage/Hero.jsx
+++ components/Landingpage/Hero.jsx
@@ -92,35 +92,51 @@ const mobileImages = ["/mob1.jpeg","/mob2.jpeg","/mob3.jpeg"];
  <div className="grid grid-cols-2 md:grid-cols-2 gap-2 sm:gap-4 mt-4 ">
 
   {/* IMAGE 1 */}
-  <div className="relative overflow-hidden rounded-xl shadow-md md:shadow-none">
+  <div className="relative overflow-hidden rounded-xl w-full h-full">
     <Image
-      src="/new2.png"
+      src="/test1.jpeg"
       width={600}
       height={350}
       alt="Hybrid B2B Marketplace in India"
       className="w-full h-[160px] sm:h-[220px] md:h-[240px] object-cover"
     />
 
-    <div className="absolute inset-0 flex flex-col justify-center p-2 sm:p-6 bg-black/10 md:bg-black/20">
+    <div className="absolute inset-0 flex flex-col justify-center pl-6  bg-black/10 ">
 
-      <h2 className="mb-3 text-white text-[19px] sm:text-lg md:text-2xl font-semibold leading-snug">
-        Looking for a product?
+      <h2 className=" text-white text-[19px] sm:text-lg md:text-2xl font-semibold leading-snug">
+        Looking for a <span className="text-[#FCD34D]">product?</span> 
       </h2>
 
-      <span className="hidden md:block text-white text-[10px] sm:text-[16px] w-full sm:w-70 mt-1 sm:mt-3 leading-snug">
-        Before making a purchase, make sure you’ve posted your buy requirement.
+      <span className="hidden md:block text-white text-[10px] sm:text-[14px] w-full sm:w-80 mt-1 sm:mt-2 leading-snug">
+       Before making a purchase, make sure you've posted your buy requirement and connect directly with verified suppliers.
       </span>
 
-      <button className="mt-2 sm:mt-4 text-white px-1 py-2 sm:px-4 sm:py-2 rounded-lg bg-[#f48335] w-fit text-[11px] sm:text-sm hover:shadow-md transition active:scale-95">
+      <button className="mt-2 sm:mt-4 mb-2 text-white px-1 py-2 sm:px-4 sm:py-2 rounded-lg bg-[#f48335] w-fit text-[11px] sm:text-sm hover:shadow-md transition active:scale-95">
         Post As Per Requirement →
       </button>
+
+<div className="flex gap-3 mt-3">
+  {/* Item 1 */}
+  <div className="flex items-center gap-2">
+    <span className="w-3 h-3 rounded-full bg-[#22d3a6]"></span>
+    <span className="text-[#8f96c9] text-xs">Direct supplier contact</span>
+  </div>
+
+  {/* Item 2 */}
+  <div className="flex items-center gap-2">
+    <span className="w-3 h-3 rounded-full bg-[#facc15]"></span>
+    <span className="text-[#8f96c9] text-xs">100% transparent</span>
+  </div>
+</div>
+
+
     </div>
   </div>
 
   {/* IMAGE 2 */}
   <div className="relative overflow-hidden rounded-xl shadow-md ">
     <Image
-      src="/new1.jpeg"
+      src="/test2.jpeg"
       width={550}
       height={300}
       alt="Hybrid B2B Marketplace in India"
@@ -131,18 +147,18 @@ const mobileImages = ["/mob1.jpeg","/mob2.jpeg","/mob3.jpeg"];
 
     <div className="relative">
   <h2 className="text-white text-[18px] sm:text-lg md:text-2xl w-full sm:w-[280px] font-semibold leading-snug">
-        Want to grow your business 10x faster?
+        Want to grow your business <span className="text-[#1E3A5F] ">10x faster? </span> 
       </h2>
 
       <div className="hidden md:flex items-center mt-1 sm:mt-3">
         <span className="text-white text-[10px] sm:text-[17px] leading-snug">
-          Showcase <br /> product
+          Showcase product
         </span>
 
         <div className="h-4 sm:h-8 border-r mx-2 sm:mx-4 border-white"></div>
 
         <span className="text-white text-[10px] sm:text-[17px] leading-snug">
-          Get Free <br /> listing
+          Get Free listing
         </span>
       </div>
 
