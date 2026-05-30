91dcd392e330947217712ff4bd6544d8128ed527
shruti0101 <shrutiguptacu@gmail.com>
2026-05-30 12:12:33 +0530

    changes

--- data/bannerData.js
+++ data/bannerData.js
@@ -10,13 +10,13 @@ const bannerData = [
         text: "Get Quotation →",
         href: "/register",
         className:
-          "border-2 border-[#F07B20]  cursor-pointer text-white font-semibold text-md px-3 py-3 rounded-xl shadow-lg transition flex items-center gap-2",
+          "border-2 border-[#F07B20]  cursor-pointer text-white font-semibold text-md px-3 py-2 rounded-xl shadow-lg transition flex items-center gap-2",
       },
       {
         text: "Partner With Us",
         href: "/post-requirement",
         className:
-          "border-2 border-cyan-400 text-white hover:bg-cyan-400/10 font-semibold text-md px-3 py-3 rounded-xl backdrop-blur-sm transition",
+          "border-2 border-cyan-400 text-white hover:bg-cyan-400/10 font-semibold text-md px-3 py-2 rounded-xl backdrop-blur-sm transition",
       },
     ],
   },
