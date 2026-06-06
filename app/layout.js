f02346a12b1590fce6078ce7a0db976aa31b689b
shruti0101 <shrutiguptacu@gmail.com>
2026-06-06 17:32:01 +0530

    changes

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Layoutwrapper from "@/components/Layout/Layoutwrapper";
 import { Toaster  } from "react-hot-toast";
 import Stickyfooter from "@/components/Stickyfooter";
+import Head from "next/head";
 
 
 const poppins = Poppins({
@@ -36,6 +37,12 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${poppins.variable} h-full antialiased`}
     >
+
+
+    <Head>
+
+      <meta name="google-site-verification" content="6bbu4UfJBXU6bw_utiN0GWbpnttBceKaSZe77oyWJME" />    
+    </Head>
 
       <body className="h-full font-sans">
       <Toaster position="top-center top-20" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
